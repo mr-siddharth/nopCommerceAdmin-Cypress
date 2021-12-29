9376def80f5ed35a710a606fe1e4323d1a7169e4
mr-siddharth <siddharth2k007@gmail.com>
2021-12-29 16:47:02 +0530

    Added package.json by npm init

--- cypress/integration/try.spec.js
+++ cypress/integration/try.spec.js
@@ -1,19 +1,19 @@
-/// <reference types="cypress-xpath" />
+// /// <reference types="cypress-xpath" />
 
-describe('my first ever cypress test suite', () => {
+// describe('my first ever cypress test suite', () => {
 
-    it("my first ever cypress test", () => {
-        cy.visit("https://admin-demo.nopcommerce.com")
-        cy.get('input#Email').clear().type("admin@yourstore.com")
-        cy.get("[for='Email']").invoke('text').then((html) => {
-            console.log("HTML:" + html)
-        })
-        cy.get('input#Password').clear().type("admin")
-        cy.get('button[type=submit]').click()
-        cy.title().should('contain', 'Dashboard')
-        cy.get('[class*="treeview"]').find('a > p').contains('Customers').parent('a').click()
-        cy.xpath("//a[@class='nav-link']/p[text()=' Customers']/parent::a").click()
-        // cy.get('[class*="treeview"]').find('a > p').contains('Customers').parent('a').parent('li').find('ul a').contains('Customers').click()
-        // cy.get('a.nav-link > p').contains('Customers').parent('a').click()
-    })
-})
+//     it("my first ever cypress test", () => {
+//         cy.visit("https://admin-demo.nopcommerce.com")
+//         cy.get('input#Email').clear().type("admin@yourstore.com")
+//         cy.get("[for='Email']").invoke('text').then((html) => {
+//             console.log("HTML:" + html)
+//         })
+//         cy.get('input#Password').clear().type("admin")
+//         cy.get('button[type=submit]').click()
+//         cy.title().should('contain', 'Dashboard')
+//         cy.get('[class*="treeview"]').find('a > p').contains('Customers').parent('a').click()
+//         cy.xpath("//a[@class='nav-link']/p[text()=' Customers']/parent::a").click()
+//         // cy.get('[class*="treeview"]').find('a > p').contains('Customers').parent('a').parent('li').find('ul a').contains('Customers').click()
+//         // cy.get('a.nav-link > p').contains('Customers').parent('a').click()
+//     })
+// })
