a5f0292c26df85dd7faeccd5e78b20b4e413444b
Siddharth Mathur <siddharth_mathur@iptiq.com>
2023-12-02 22:54:14 +0530

    Upgraded to Cypress 13

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
+  video: false,
+  projectId: '8kfavr',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://admin-demo.nopcommerce.com',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
